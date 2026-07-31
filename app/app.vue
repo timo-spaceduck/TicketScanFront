60878fa48da774cf3d6a78e46b424ec7c9d0d14c
Timo <timofey.khmelev@gmail.com>
2026-07-31 10:47:55 +0500

    seo stuff

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,18 @@
 <script setup>
+const url = useRequestURL()
+
 useHead({
   meta: [
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { name: 'theme-color', content: '#ffffff' }
   ],
   link: [
-    { rel: 'icon', href: '/favicon.ico' }
+    { rel: 'icon', href: '/favicon.ico', sizes: '48x48' },
+    { rel: 'icon', href: '/favicon.svg', type: 'image/svg+xml' },
+    { rel: 'icon', href: '/favicon-96x96.png', type: 'image/png', sizes: '96x96' },
+    { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+    { rel: 'manifest', href: '/site.webmanifest' },
+    { rel: 'canonical', href: `https://ticket-scan.com${url.pathname}` }
   ],
   htmlAttrs: {
     lang: 'en'
